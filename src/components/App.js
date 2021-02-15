029a268d620dd3029786660757a754a51caa8f04
Hubert <hubertworwa1993@gmail.com>
2021-02-15 23:31:47 +0100

    Connect to Apollo Server

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles.css";
+import RandomQuote from "./RandomQuote";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Inspiring quote</h1>
+      <RandomQuote />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
 
-import App from "./App";
+import App from "./components/App";
+
+const URL = "https://fathomless-fortress-74026.herokuapp.com";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: URL,
+  }),
+  queryDeduplication: false,
+});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
   rootElement
 );
